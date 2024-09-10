c541799e9a88d8dfcea0075404c8719d8c0d221a
osanaga.haruki <osanaga.haruki.me6@jp.nssol.nipponsteel.com>
2024-09-10 18:35:05 +0900

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import { generateClient } from "aws-amplify/data";
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
@@ -17,13 +20,22 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
   return (
+    <Authenticator>
+      {({ signOut }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -34,6 +46,8 @@ function App() {
         </a>
       </div>
     </main>
+    )}
+    </Authenticator>
   );
 }
 
